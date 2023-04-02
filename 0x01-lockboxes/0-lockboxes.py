15f8d6129f9956911789d514913f7148c0b3d421
erickadikah <erickadikah2030@gmail.com>
2023-04-02 14:04:55 +0300

    locked boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,21 +4,11 @@
 
 def canUnlockAll(boxes):
     """checking the lenth of the boxes"""
-    if(len(boxes) == 0 or type(boxes) != list):
-        """initilization for unchecked boxes"""
-        return false
+    n = len(boxes)
+    b_unlocked = [0]
+    for  box_num, box in enumerate(boxes):
+        for key in box:
+            if key < n and key not in b_unlocked and key != box_num:
+                b_unlocked.append(key)
 
-    b_locked = len(boxes)
-    keys = [0]
-    for key in keys :
-        for box in boxes[key]:
-            if (box != 0 and box < b_locked):
-                keys.append(box)
-
-
-    keys = set(keys)
-
-    if (len(keys) == len(boxes)):
-        return True
-    else:
-        return False
+    return len(b_unlocked) == n
