7ed7c027b0d705bfd8c08836b436368e008c025e
erickadikah <erickadikah2030@gmail.com>
2023-04-05 17:40:47 +0300

    explanation of lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,6 +6,9 @@ def canUnlockAll(boxes):
     """checking the lenth of the boxes"""
     n = len(boxes)
     b_unlocked = [0]
+    """b_unlocked for keeping track for the unlocked boxes
+        enumarate(boxes) iterates over the boxes
+    """
     for  box_num, box in enumerate(boxes):
         for key in box:
             if key < n and key not in b_unlocked and key != box_num:
