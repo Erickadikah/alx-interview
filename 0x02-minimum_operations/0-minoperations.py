cb46941315dc68ea9021eaa73e51cae35446da2e
erickadikah <erickadikah2030@gmail.com>
2023-04-06 15:16:22 +0300

    erro fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,21 +6,15 @@
 
 
 def minOperations(n):
-    """checks if n is less than if it is returns 0"""
+    """checks if n is less than if it is returns 0
+        keeping track of number of operation perfomed
+    """
     if n < 2:
         return 0
-    """keeping track of number of operation perfomed"""
     ops = 0
     divisor = 2
     while n > 1:
-        """keeps executing untill n is educed to 1 rep
-            desired string legth
-        """
         if n % divisor == 0:
-            """checks if divisor is a factor of n
-                represents a copy operation
-                continues until divisor in not afactor of n
-            """
             ops += divisor
             n /= divisor
         else:
