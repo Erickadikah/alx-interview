ed72dc1d58b61b981e42287694262421cadd659e
erickadikah <erickadikah2030@gmail.com>
2023-04-14 00:35:37 +0300

    fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -22,9 +22,9 @@ try:
     for line in stdin:
         counter += 1
         status_code = int(line.split()[-2])
-        file_size = int(line.split()[-1])
         if status_code in status_code_counts:
             status_code_counts[status_code] += 1
+            file_size = int(line.split()[-1])
             total_size += file_size
         if counter % 10 == 0:
             print("File size: {}".format(total_size))
