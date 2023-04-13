df14c61572a83d53d67ab117f422a711e4bc620b
erickadikah <erickadikah2030@gmail.com>
2023-04-14 02:24:00 +0300

    try2

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -22,7 +22,7 @@ try:
     for line in stdin:
         counter += 1
         status_code = int(line.split()[-2])
-        if status_code in status_code_counts:
+        if status_code in status_code_counts and len(line.split()) == 9:
             status_code_counts[status_code] += 1
             file_size = int(line.split()[-1])
             total_size += file_size
