b479321d0eb62107c1800d51de9bdaa795980357
erickadikah <erickadikah2030@gmail.com>
2023-04-20 21:43:14 +0300

    method 2 utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,39 +1,48 @@
 #!/usr/bin/python3
-"""determines if a given data set represents a valid UTF-8 encoding.
+""" This is method 2 of the UTF-8 Validation problem
+    - This method is less efficient than method 1
+    - This method usses hexadecimals instead of binary
+    - to check if an integer is a valid UTF-8 character
+    - in the int[data] being passed in
 """
 
 
 def validUTF8(data):
-    """Args: data[int]
-        Reurns: True if data is a valid UTF-8 encoding,
-        else return False
-        '>>' is the bitwise right shift operator
-        shifting the bits of the number to the right
-        resulsting to a new byte where MSB is still 0
-        resulting to true if the number is 0
-        byte_number: keep track of the number of bytes
-        in the current UTF-8 character
-        (byte >> 7) == 0: 1 byte character
-        (byte >> 5) == 0b110: 2 byte character
-        (byte >> 4) == 0b1110: 3 byte character
-        (byte >> 3) == 0b11110: 4 byte character
-        (byte >> 2) == 0b111110: 5 byte character
     """
-    bytes_number = 0
-    for byte in data:
-        if bytes_number == 0:
-            if (byte >> 7) == 0:
-                continue
-            elif (byte >> 5) == 0b110:
-                bytes_number = 1
-            elif (byte >> 4) == 0b1110:
-                bytes_number = 2
-            elif (byte >> 3) == 0b11110:
-                bytes_number = 3
-            else:
+        Args: data[int]
+        Each interger reprsents 1byte
+        Return: True if data is a valid UTF-8 encoding,
+        else return False
+        0x80 = 10000000: 1 byte character: 128
+        0xC0 = 11000000: 2 byte character: 192
+        0xE0 = 11100000: 3 byte character: 224
+        0xF0 = 11110000: 4 byte character: 240
+        we are checking if the first byte is a valid UTF-8 character
+        if it is, we check the next byte to see
+          if it is a valid UTF-8 character
+        in the case of a 3 byte character, we check the next 2 bytes
+    """
+    i = 0
+    while i < len(data):
+        if data[i] < 128:
+            i += 1
+        elif data[i] < 192:
+            return False
+        elif data[i] < 224:
+            if i + 1 >= len(data) or (data[i + 1] & 192) != 128:
                 return False
+            i += 2
+        elif data[i] < 240:
+            if i + 2 >= len(data) or (data[i + 1] &
+                                      192) != 128 or (data[i + 2] &
+                                                      192) != 128:
+                return False
+            i += 3
+        elif data[i] < 248:
+            if i + 3 >= len(data) or (data[i + 1] & 192) != 128 or (
+                    data[i + 2] & 192) != 128 or (data[i + 3] & 192) != 128:
+                return False
+            i += 4
         else:
-            if (byte >> 6) != 0b10:
-                return False
-            bytes_number -= 1
-    return bytes_number == 0
+            return False
+    return True
