3cf07bbde2af5e6a86cd627663b4fcacd473eb13
erickadikah <erickadikah2030@gmail.com>
2023-05-04 01:53:09 +0300

    starwars api implementation

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 /*
 This script prints all character of Star Wars movie
+position arguent : Movie ID
 */
 const request = require('request');
 
@@ -17,21 +18,38 @@ request(url, function (error, _response, body) {
   const charatersUrls = data.characters;
 
   if (!charatersUrls || !Array.isArray(charatersUrls)) {
-    console.log('Error: charactor not found not in the  array');
+    console.log('Error: character not found not in the  array');
   }
   const charactersMap = new Map();
 
+    {/*
+    getting character for each chracter filtered from character url
+    checking for character in each Url
+    */}
   charatersUrls.forEach((url, index) => {
     request(url, function (error, _response, body) {
       if (error) {
         console.log(error);
         return;
       }
+
+    {/*
+      variable pass characters in json format
+      mapping characters in set of name and order
+      :according to character index
+    */}
+
       const character = JSON.parse(body);
       charactersMap.set(url, {
         name: character.name,
         order: index
       });
+
+      {/*
+        check if all charactersUrls have been processed by request
+        and pass the to sortedCharacters array then sorted in order property
+        in ascending order , and print each in an ascending order
+    */}
 
       if (charactersMap.size === charatersUrls.length) {
         const sortedCharacters = Array.from(charactersMap.values()).sort((a, b) => a.order - b.order);
