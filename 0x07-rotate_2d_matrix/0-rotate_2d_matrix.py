4eb1bde9c73bacd6878aa4b3a76ae1212af50ef2
erickadikah <erickadikah2030@gmail.com>
2023-05-11 01:08:19 +0300

    rotate_2D matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Rotate 2D matrix"""
+
+def rotate_2d_matrix(matrix):
+    """Rotate 2D matrix implementation
+    """
+    n = len(matrix)
+    # transpose the metrix
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    # reverse row
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
+    return matrix
