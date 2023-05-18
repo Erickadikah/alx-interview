04892f0f81201e24851a09bf9b3a1494ad3cb673
erickadikah <erickadikah2030@gmail.com>
2023-05-18 16:29:40 +0300

    make change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,39 +3,24 @@
 
 def makeChange(coins: int, total: int):
     """ARGS: Coins, total
-        function uses high denoination count
+        function that determines the fewest number of coins
     """
-    memo = {}  # Memoization dictionary to store computed results
+    counter = 0
+    if total <= 0:
+        return 0
+    if not coins:
+        return -1
+    coins.sort(reverse=True)
+    for coin in coins:
+        if total <= 0:
+            break
+        while total >= coin:
+            total -= coin
+            counter += 1
+    if total != 0:
+        return -1
+    return counter
 
-    def dp(amount):
-        # Check if amount is in memo
-        if amount in memo:
-            return memo[amount]
-
-        if amount == 0:
-            return 0
-
-        if amount < 0:
-            return -1
-
-        minCoins = float('inf')  # Initialize with infinity
-
-        for coin in coins:
-            subproblem = dp(amount - coin)
-            if subproblem == -1:
-                continue
-
-            minCoins = min(minCoins, subproblem + 1)
-
-        memo[amount] = minCoins if minCoins != float('inf') else -1
-        return memo[amount]
-
-    return dp(total)
-    # counter = 0
-    # if total <= 0:
-    #     return 0
-    # if not coins:
-    #     return -1
 
     # for coin in sorted(coins, reverse=True):
     #     count =int(total // coin)
